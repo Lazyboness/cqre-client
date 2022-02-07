8fb3fe0766537f97598ead93953d427bb30d8f74
Lazyboness <jaeyoon4819@gmail.com>
2022-02-07 10:20:54 +0900

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
+import ProjectPage from './pages/ProjectPage';
+import BoardPage from './pages/BoardPage';
+import ContactPage from './pages/ContactPage';
+import ProductPage from './pages/ProductPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/project" component={ProjectPage} />
+        <Route exact path="/board" component={BoardPage} />
+        <Route exact path="/contact" component={ContactPage} />
+        <Route exact path="/product" component={ProductPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
